ac807de14bb2656e72583137cf80e8790fbf2427
praveen-hb <praveenudu9@gmail.com>
2020-04-16 10:43:05 +0530

    Menu table created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_15_144932) do
+ActiveRecord::Schema.define(version: 2020_04_16_044956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,20 +22,7 @@ ActiveRecord::Schema.define(version: 2020_04_15_144932) do
     t.integer "price"
   end
 
-  create_table "todos", force: :cascade do |t|
-    t.text "todo_text"
-    t.date "due_date"
-    t.boolean "completed"
-    t.bigint "user_id"
+  create_table "menus", force: :cascade do |t|
+    t.string "name"
   end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
-    t.string "first_name"
-    t.string "last_name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
 end

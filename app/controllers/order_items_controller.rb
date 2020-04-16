af66182c8b735f97bf094a1e79dda07ac42d1f34
poojitha2512 <pooji.kanamarlapudi@gmail.com>
2020-04-16 14:58:22 +0530

    order_items create function added

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,2 +1,14 @@
 class OrderItemsController < ApplicationController
+  def create
+    @temp = menu_items.selected_ones
+    @temp.each do |t|
+      OrderItem.create!(
+        order_id: 1,
+        menu_item_id: t.menu_item_id,
+        menu_item_name: t.menu_item_name,
+        menu_item_price: t.menu_item_price,
+      )
+    end
+    render plain: "order items created"
+  end
 end
--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,3 +1,6 @@
 class MenuItem < ApplicationRecord
   belongs_to :menu
+  def self.selected_ones
+    all.where(selected: true)
+  end
 end

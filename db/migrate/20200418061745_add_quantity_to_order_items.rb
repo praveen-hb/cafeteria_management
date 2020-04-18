1a0646ca54391afd0a2cb2a5ebbefe218a4cb585
praveen-hb <praveenudu9@gmail.com>
2020-04-18 12:11:18 +0530

    cart total added

---
+++ db/migrate/20200418061745_add_quantity_to_order_items.rb
@@ -0,0 +1,5 @@
+class AddQuantityToOrderItems < ActiveRecord::Migration[6.0]
+  def change
+    add_column :order_items, :menu_item_quantity, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_17_191951) do
+ActiveRecord::Schema.define(version: 2020_04_18_062127) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2020_04_17_191951) do
     t.integer "menu_item_id"
     t.string "menu_item_name"
     t.integer "menu_item_price"
+    t.integer "menu_item_quantity"
   end
 
   create_table "orders", force: :cascade do |t|
